c21a645c3c85f6dcee9fa492fda7fb77d2e5aa0e
Clelton <cleltonjm_95@hotmail.com>
2024-08-21 21:21:54 -0300

    Configuração do Stiches

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { colors } from '@ignite-ui/tokens'
+
+export function App() {
+    return (
+        <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
+    )
+}
